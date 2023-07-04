18138c586e48f1d5722b32e4559f8b1e30039889
Cynthia Mwihaki Kamau <cyndiekamaa@gmail.com>
2023-07-04 12:02:09 +0300

    strings, constants, integers

--- first_project/src/main.rs
+++ first_project/src/main.rs
@@ -3,6 +3,11 @@
 Rust tutorial for practice
 By Cyndie :)
  */
+#![allow(unused)]
+
+const GLOBAL: &str = "constants are global vars";
+const BIRTHDAY: i32 = 1_i32;
+
 fn main() {
     println!("Hello, world!");  // 1. println! is a macro
 
@@ -29,7 +34,47 @@ fn main() {
     let mut hello2 = "hello world again";
     println!("{}", hello2);
 
-    let hello2 = "hello once more";
+    hello2 = "hello once more";
     println!("{}", hello2);
+
+    // CONSTANTS -> have to be in UPPERCASE. Are global (occur outside of function)
+
+    println!("{}", GLOBAL);
+
+
+    // SUFFIXES -> u32, u64, u8 etc
+
+    let x = 20_u32;
+    let y = 1_000;
+
+    println!("{} + {} = {}", x, y, x+y);
+
+    // BIRTHDAY QUIZ
+
+    birthday();
+
+
+
+
 }
 
+ /* Challenge 1 - Build a program that has the following:
+
+    1) Has a global constant integer named 'birthday' with a value of 1
+    2) Has a local string variable named 'my_name' with your name as the value
+    3) Has a local string variable named 'my_birthday' with your birth month/day (no year) as the value
+    4) Has a local mutable integer variable named 'age' with your current age as the value
+    5) Has a local integer variable named 'new_age' with your age after your birthday as the value
+    6) Prints out 'My name is X and I am X years old. I will turn X on X' 
+
+*/
+
+fn birthday() {
+    let my_name = "Cyndie";
+    let my_birthday: &str = "13th October";
+    let mut age = 23_i32;
+    let new_age = age + BIRTHDAY;
+
+    println!("My name is {} and I am {} years old. I will turn {} on {} this year", my_name, age, new_age, my_birthday);
+
+}
